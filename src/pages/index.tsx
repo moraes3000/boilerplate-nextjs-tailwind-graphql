9fd0d722052929b9ddaac917a1e54898677283df
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-08-04 13:59:57 -0300

    Config de prettier para auto identarção

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <div className='max-w-[900px] mx-auto'>
-    <h1 className='text-3xl font-bold my-4'>Home</h1>
-    <ul>
-      <li> <Link href={`/modelo-axios/`}><a>Modelo Axios</a></Link></li>
-      <li> <Link href={`/graphql_request/`}><a>GraphQL</a></Link></li>
-    </ul>
+      <h1 className='text-3xl font-bold my-4'>Home</h1>
+      <ul>
+        <li> <Link href={`/modelo-axios/`}><a>Modelo Axios</a></Link></li>
+        <li> <Link href={`/graphql_request/`}><a>GraphQL</a></Link></li>
+      </ul>
     </div>
   )
 }
